ae80090276a6f5792a1bd2df81838e6a3f6e2608
alligator <enemy.forest.brigade@gmail.com>
2021-12-20 22:45:32 +0000

    add -t flag to run-all

--- aoc-lang/run-all.py
+++ aoc-lang/run-all.py
@@ -1,11 +1,15 @@
 import subprocess
 import glob
 import re
+import sys
 
 pat = re.compile(r'part[12] took (.*)')
 
 for f in sorted(glob.glob('*.aoc')):
-    p = subprocess.run(f'aoc21 -b {f}', shell=True, capture_output=True)
+    cmd = f'aoc21 -b {f}'
+    if '-t' in sys.argv:
+        cmd = f'aoc21 -b -t {f}'
+    p = subprocess.run(cmd, shell=True, capture_output=True)
     m = pat.findall(p.stdout.decode('utf-8'))
     if m:
         p1 = m[0]
